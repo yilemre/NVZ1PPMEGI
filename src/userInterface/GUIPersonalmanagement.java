88de8149230c3498e0c03a13ccb4d7b620bfbe62
Your Name <marius.schmidt1@icloud.com>
2018-05-28 20:11:28 +0200

    little fix

--- src/userInterface/GUIPersonalmanagement.java
+++ src/userInterface/GUIPersonalmanagement.java
@@ -654,8 +654,7 @@ frmElabVerwaltungsprogramm.setVisible(true);
 		if (command=="Person hinzuf\u00FCgen") {
 		    //Emre begin
 		    
-		    try { 
-			System.out.println("test");
+		    try {
 			PersonManagement.addPerson(textFieldname.getText(), textFieldlastName.getText(), textFieldStreet.getText(), Integer.parseInt(textFieldhouseNumber.getText()),
 				Integer.parseInt(textFieldzipCode.getText()),textFieldeMail.getText(), textFielduserName.getText(), String.valueOf(passwordField.getPassword()), comboBoxType.getSelectedIndex());
 		    } 
