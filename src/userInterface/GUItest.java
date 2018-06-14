bd6b353e9809b64658e53c0c21ff91e900c369c7
Marius Schmidt <marius2.schmidt@student.uni-siegen.de>
2018-06-14 22:38:00 +0200

    changed opening GUI

--- src/userInterface/GUItest.java
+++ src/userInterface/GUItest.java
@@ -6,7 +6,7 @@ public class GUItest {
 		// TODO Auto-generated method stub
 
 		
-		GuiMenue gui = new GuiMenue();
+		GuiLogin gui = new GuiLogin();
 		
 		//GUIRegisterCustomer gui = new GUIRegisterCustomer();
 		
